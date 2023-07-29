cae8f33767550904d0c2fd3f8a333e444b929ae1
Herberth Kagiya <tayki.heberth@gmail.com>
2023-07-29 13:25:21 -0300

    Inital Commit

---
+++ app/src/main/java/com/kagiya/solarium/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.kagiya.solarium
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kagiya.solarium.databinding.ActivityMainBinding
+import com.kagiya.solarium.ui.theme.SolariumTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
